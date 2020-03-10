9a5ff6737a7c0447d90ec3035256ba4b20a3e32e
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-09 17:38:34 -0700

    Simpler call to plotting

--- example.py
+++ example.py
@@ -10,8 +10,8 @@ import numba
 from illustration import *
 from integrator import *
 
-MAKE_VIDEO=True
-SAVEFIG=True
+MAKE_VIDEO= True
+SAVEFIG   = True
 
 ex = Experiment('SpringBox')
 if SAVEFIG or MAKE_VIDEO:
@@ -37,15 +37,14 @@ def cfg():
 
 @ex.automain
 def main(AR, n_part, cutoff, dt, m,T,k, savefreq, L, drag_factor,lower_cutoff, r0):
-    imagefolder = f'/tmp/boxspring-{int(time.time())}'
-    os.makedirs(imagefolder)
+    image_folder = f'/tmp/boxspring-{int(time.time())}'
+    os.makedirs(image_folder)
     pXs = (np.random.rand(n_part,2)-.5)*2*L
     pVs = np.zeros_like(pXs)
     for i in tqdm(range(int(T/dt))):
         pXs, pVs, fXs, fVs = integrate_one_timestep(pXs, pVs, dt=dt, m=m,cutoff=cutoff,lower_cutoff=lower_cutoff,k=k,AR=AR, drag_factor=drag_factor, r0=r0, L=L)
         if savefreq!=None and i%savefreq == 0:
-            plot_points(pXs, pVs, i, cutoff=cutoff,lower_cutoff=lower_cutoff, image_folder=imagefolder, title=f'cutoff={cutoff:.2f}, t={i*dt:.3f}', AR=AR, L=L, SAVEFIG=SAVEFIG, ex=ex)
-            plot_fluid(fXs, fVs, i, imagefolder, title=f'cutoff={cutoff:.2f}, t={i*dt:.3f}',L=L, SAVEFIG=False)
+            plot_data(pXs, pVs, fXs, fVs, i, image_folder=image_folder, title=f't={i*dt:.3f}', L=L, fix_frame=True, SAVEFIG=SAVEFIG, ex=ex, plot_particles=True, plot_fluids=True, side_by_side=True)
     if MAKE_VIDEO:
-        video_path = generate_video_from_png(imagefolder)
+        video_path = generate_video_from_png(image_folder)
         ex.add_artifact(video_path)
--- illustration.py
+++ illustration.py
@@ -1,39 +1,45 @@
-# Adapted from answer by BoboDarph (Stackoverflow: https://stackoverflow.com/questions/44947505/how-to-make-a-movie-out-of-images-in-python)
-
 import cv2
 import os
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import numpy as np
 
-def plot_fluid(fXs, fVs,i,  image_folder, title,L,fix_frame=True, SAVEFIG=True, ex=None):
-    fig=plt.figure(figsize=(10,10))
+def plot_fluid(ax, fXs, fVs):
+    plt.sca(ax)
     vabs = np.linalg.norm(fVs,axis=1)
+    plt.title('Fluids')
     X = np.linspace(min(fXs.flatten()),max(fXs.flatten()),int(np.sqrt(len(fXs)))) ## a bit of a hack, but it works for now
     X,Y = np.meshgrid(X,X)
-    qc=plt.quiver(X,Y, fVs[:,0],fVs[:,1], vabs , units='xy', pivot='mid', scale=.1)
-    plt.title(title)
-    if fix_frame:
-        plt.xlim([-L,L])
-        plt.ylim([-L,L])
-    IMG_NAME=f'{image_folder}/fluid{i:08}.png'
-    plt.savefig(IMG_NAME)
-    if SAVEFIG:
-        ex.add_artifact(IMG_NAME)
-    try:
-        plt.close(fig)
-    except:
-        print('Something went wrong with closing the figure')
+    qc=plt.quiver(X,Y, fVs[:,0],fVs[:,1], vabs , units='xy', pivot='mid', scale=1)
 
-def plot_points(pXs, pVs, i,cutoff,lower_cutoff, image_folder, title, AR,L, fix_frame=True, SAVEFIG=True, ex=None):
-    fig=plt.figure(figsize=(12,10))
+def plot_points(ax, pXs, pVs):
+    plt.sca(ax)
+    plt.title('Particles')
     vabs = np.linalg.norm(pVs, axis=1)
     sc=plt.scatter(pXs[:,0],pXs[:,1], c=vabs, cmap=plt.get_cmap('viridis'), vmin=0, vmax=max(vabs))
-    plt.colorbar(sc)
-    plt.title(title)
+
+def plot_data(pXs, pVs, fXs, fVs, i, image_folder, title, L, fix_frame=True, SAVEFIG=True, ex=None, plot_particles=True, plot_fluids=True, side_by_side=False):
+    if not (plot_particles or plot_fluids):
+        print('Plotting without anyting to plot. Raising exception...')
+        raise RuntimeError
+    if side_by_side:
+        fig, (axl,axr) = plt.subplots(1,2, figsize=(10,5))
+    else:
+        fig = plt.figure(figsize=(5,5))
+        axl = fig.gca()
+        axr = fig.gca()
+
+    if plot_particles:
+        plot_points(axl, pXs, pVs)
+    if plot_fluids:
+        plot_fluid(axr, fXs, fVs)
+
+    fig.suptitle(title)
     if fix_frame:
         plt.xlim([-L,L])
         plt.ylim([-L,L])
+    plt.tight_layout()
+
     IMG_NAME=f'{image_folder}/fig{i:08}.png'
     plt.savefig(IMG_NAME)
     if SAVEFIG:
@@ -44,6 +50,7 @@ def plot_points(pXs, pVs, i,cutoff,lower_cutoff, image_folder, title, AR,L, fix_
         print('Something went wrong with closing the figure')
 
 def generate_video_from_png(image_folder):
+# Adapted from answer by BoboDarph (Stackoverflow: https://stackoverflow.com/questions/44947505/how-to-make-a-movie-out-of-images-in-python)
     images = sorted([img for img in os.listdir(image_folder) if img.endswith(".png")])
     if len(images)>0:
         frame = cv2.imread(os.path.join(image_folder, images[0]))
