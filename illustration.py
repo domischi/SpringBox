4805c9381a206f14a5f9e52228de19e8821d8dda
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-10 09:48:59 -0700

    Added the IO parameter coloring to the fluid plot. With the limited amount of data however, it seems that it is not very informative

--- illustration.py
+++ illustration.py
@@ -4,19 +4,45 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import numpy as np
 
-def plot_fluid(ax, fXs, fVs, plot_type='streamplot'):
+def get_fluid_colors(fXs, fVs, normalize=False):
+    ## I realized that with limited data, this coloring does not make much sense
+    """
+    Given the velocity field (Ux, Uy) at positions (X,Y), compute for every x in (X,Y) if the position is contributing to the inflow or the outflow. This can be obtained by u*x (scalar product). If this quantity is positive, then it's an outflow, if it is negative it's an inflow.
+    """
+    ret = [np.dot(x,v) for x,v in zip(fXs, fVs)]
+    if normalize:
+        mi=min(ret)
+        mx=max(ret)
+        assert(mi<0 and mx>0)
+        ret = [r/mi if r<0 else r/mx for r in ret]
+    return np.array(ret)
+
+
+def plot_fluid(ax, fXs, fVs, plot_type='streamplot', coloring_scheme='vabs'):
     plt.sca(ax)
-    vabs = np.linalg.norm(fVs,axis=1)
     plt.title('Fluids')
     ng = int(np.sqrt(len(fXs)))
     X = np.linspace(min(fXs.flatten()),max(fXs.flatten()), ng) ## a bit of a hack, but it works for now
+    if coloring_scheme == 'io':
+        c = get_fluid_colors(fXs,fVs)
+        cmap = 'bwr'
+    elif coloring_scheme == 'solid':
+        c = np.ones(len(fXs))
+        cmap = 'binary'
+    elif coloring_scheme == 'vabs':
+        c = np.linalg.norm(fVs,axis=1)
+        cmap = 'viridis'
+    else: 
+        print('Illegal coloring_scheme in fluid plot. Check the code.')
+        return
     if plot_type == 'quiver':
         X,Y = np.meshgrid(X,X)
-        p=plt.quiver(X,Y, fVs[:,0],fVs[:,1], vabs , units='xy', pivot='mid')
+        p=plt.quiver(X,Y, fVs[:,0],fVs[:,1], c, cmap=cmap , units='xy', pivot='mid')
     elif plot_type == 'streamplot':
-        p=plt.streamplot(X,X, fVs[:,0].reshape(ng,ng),fVs[:,1].reshape(ng,ng), color=vabs.reshape(ng,ng))
+        p=plt.streamplot(X,X, fVs[:,0].reshape(ng,ng),fVs[:,1].reshape(ng,ng), color=c.reshape(ng,ng), cmap=cmap)
     else:
         print('Illegal type of fluid plot. Check the code.')
+        return
 
 def plot_points(ax, pXs, pVs):
     plt.sca(ax)
