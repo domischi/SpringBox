5e6e5ea92cc45dffa3b5caa672a8e8e075ef0e11
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-11 14:19:10 -0700

    Implement feature to interpolate fluid forces on a grid and apply those to the particles

--- example.py
+++ example.py
@@ -39,20 +39,23 @@ def cfg():
     savefreq = 10
     r0=0.2
     drag_factor=1
+    # Speeds up the computation somewhat, but incurs an error due to oversmoothing of fluids (which could however be somewhat physical)
+    use_interpolated_fluid_velocities = True
 
 #TODO go for less magic....
 @ex.automain
-def main(run_id, AR, n_part, cutoff, dt, m,T,k, savefreq, L, drag_factor,lower_cutoff, r0, mu, Rdrag):
+def main(run_id, AR, n_part, cutoff, dt, m,T,k, savefreq, L, drag_factor,lower_cutoff, r0, mu, Rdrag, use_interpolated_fluid_velocities):
     timestamp = int(time.time())
     image_folder = f'/tmp/boxspring-{run_id}-{timestamp}'
     os.makedirs(image_folder)
     pXs = (np.random.rand(n_part,2)-.5)*2*L
     pVs = np.zeros_like(pXs)
+    if use_interpolated_fluid_velocities:
+        print('WARNING: Using interpolated fluid velocities can yield disagreements. The interpolation is correct for most points. However, for some the difference can be relatively large.')
     time.sleep(3) # To have the output of all other runs finished
     for i in tqdm(range(int(T/dt)), position=run_id):
-    #for i in tqdm(range(int(T/dt))):
         plotting_this_iteration = savefreq!=None and i%savefreq == 0
-        pXs, pVs, fXs, fVs = integrate_one_timestep(pXs, pVs, dt=dt, m=m,cutoff=cutoff,lower_cutoff=lower_cutoff,k=k,AR=AR, drag_factor=drag_factor, r0=r0, L=L, mu=mu, Rdrag=Rdrag, get_fluid_velocity=plotting_this_iteration)
+        pXs, pVs, fXs, fVs = integrate_one_timestep(pXs, pVs, dt=dt, m=m,cutoff=cutoff,lower_cutoff=lower_cutoff,k=k,AR=AR, drag_factor=drag_factor, r0=r0, L=L, mu=mu, Rdrag=Rdrag, get_fluid_velocity=plotting_this_iteration, use_interpolated_fluid_velocities=use_interpolated_fluid_velocities)
         if plotting_this_iteration:
             plot_data(pXs, pVs, fXs, fVs, i, image_folder=image_folder, title=f't={i*dt:.3f}', L=L, fix_frame=True, SAVEFIG=SAVEFIG, ex=ex, plot_particles=True, plot_fluids=True, side_by_side=True, fluid_plot_type = 'quiver')
     if MAKE_VIDEO:
--- integrator.py
+++ integrator.py
@@ -1,6 +1,8 @@
 import numba
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
+from scipy.interpolate import RectBivariateSpline
+import matplotlib.pyplot as plt
 
 @numba.njit(parallel=True)
 def point_in_active_region(ps, AR):
@@ -22,8 +24,12 @@ def RHS(pXs, cutoff, lower_cutoff,k, AR, r0):
     return rhs
 
 @numba.jit
+def get_linear_grid(L,res=32):
+    return np.linspace(-L,L,res)
+
+@numba.jit
 def get_grid_pairs(L,res=32):
-    gridX = np.linspace(-L,L,32)
+    gridX = get_linear_grid(L,res)
     XY = np.array(np.meshgrid(gridX,gridX)).reshape(2,res*res).T
     return XY
 
@@ -40,19 +46,41 @@ def fVs_on_points(ps, pXs, pVs, mu=1):
     return fVs/(8*np.pi*mu)
 
 @numba.jit
-def fVs_on_grid(pXs, pVs, L, mu=1):
-    fXs = get_grid_pairs(L)
+def fVs_on_grid(pXs, pVs, L, mu=1, res=32):
+    fXs = get_grid_pairs(L, res)
     return fXs, fVs_on_points(fXs, pXs, pVs, mu=mu)
 
 @numba.jit
-def integrate_one_timestep(pXs, pVs, dt, m , cutoff, lower_cutoff, k, AR, drag_factor, r0, L, mu, Rdrag, get_fluid_velocity=False):
+def fVs_on_particles(pXs, pVs, L, mu=1, res=32, spline_degree=3):
+    fXs_grid, fVs_grid = fVs_on_grid(pXs, pVs, L, mu, res=res)
+    gridX = get_linear_grid(L,res)
+    func_fV_x = RectBivariateSpline(gridX, gridX, fVs_grid[:,0].reshape(res,res).T, kx=spline_degree, ky=spline_degree) # Currently using a spline_degreerd degree splin
+    func_fV_y = RectBivariateSpline(gridX, gridX, fVs_grid[:,1].reshape(res,res).T, kx=spline_degree, ky=spline_degree)
+    fVs_x = func_fV_x.ev(pXs[:,0], pXs[:,1])
+    fVs_y = func_fV_y.ev(pXs[:,0], pXs[:,1])
+    return np.array((fVs_x,fVs_y)).T
+
+@numba.jit
+def integrate_one_timestep(pXs, pVs, dt, m , cutoff, lower_cutoff, k, AR, drag_factor, r0, L, mu, Rdrag, get_fluid_velocity=False, use_interpolated_fluid_velocities=True, DEBUG_INTERPOLATION=False):
     pXs = pXs + dt * pVs
     pVs = (1-drag_factor)*pVs + dt/m * RHS(pXs,cutoff=cutoff, lower_cutoff=lower_cutoff,k=k, AR=AR, r0=r0)
     if Rdrag > 0:
         # Rdrag determines the size of the fluid to object coupling. Each of the particles is assumed to follow 3D Stokes drag (F_d=6*pi*mu*Rdrag*v) where Rdrag is the radius of an equivalent sphere
     # This might be a bad approximation due to Stokes paradox, stating that the problem is ill defined in 2D
     # The drag in the 2D case is rather radius independent and depends on the Reynolds number. However, since all particles are assumed to be the same, and the Reynolds number stays constant as well, this parametrization is still valid.
-        pVs += 6*np.pi*mu*Rdrag*fVs_on_points(pXs, pXs, pVs, mu=mu) # TODO implement this as a interpolation task rather than computing it on each and every particle separately.
+        if use_interpolated_velocities:
+            fVs = fVs_on_particles(pXs, pVs, L, res=32, spline_degree=3)
+        else:
+            fVs = fVs_on_points(pXs, pXs, pVs)
+        if DEBUG_INTERPOLATION:
+            if use_interpolated_velocities:
+                fVs2 = fVs_on_points(pXs, pXs, pVs)
+            else:
+                fVs2 = fVs_on_particles(pXs, pVs, L, res=32, spline_degree=3)
+            plt.quiver(pXs[:,0],pXs[:,1], fVs_interp[:,0], fVs_interp[:,1], color='red')
+            plt.quiver(pXs[:,0],pXs[:,1], fVs_direct[:,0], fVs_direct[:,1], color='green')
+            plt.show(block=True)
+        pVs += 6*np.pi*mu*Rdrag*fVs_on_particles(pXs, pVs, L, mu=mu)
     if get_fluid_velocity:
         fXs, fVs = fVs_on_grid(pXs, pVs, L, mu=mu)
         return pXs, pVs, fXs, fVs
