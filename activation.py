db0859dbf9771fd6f385577632b75cb609ed42dc
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-17 15:33:09 -0700

    Changed the name of the rectangular activation function

--- activation.py
+++ activation.py
@@ -2,12 +2,12 @@ import numba
 import numpy as np
 
 @numba.njit(parallel=True)
-def point_in_active_region(ps, AR):
+def rectangle_activation(ps, AR):
     return (ps[:,0]>-1) * (ps[:,0]<1) * (ps[:,1]>-AR) * (ps[:,1]<AR)
 
 
 def activation_fn_dispatcher(_config, t):
     if _config['activation_fn_type'] == 'const-rectangle':
-        return lambda ps: point_in_active_region(ps, _config['AR'])
+        return lambda ps: rectangle_activation(ps, _config['AR'])
     else:
         raise RuntimeError('Unrecognized activation_fn_type')
