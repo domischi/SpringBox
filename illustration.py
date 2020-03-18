5d56824ecce41d6fa50ffa32f9bb7f90b423710e
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-18 16:23:55 -0700

    Use the sim_info dictionary throughout the script

--- illustration.py
+++ illustration.py
@@ -52,7 +52,7 @@ def plot_points(ax, pXs, pVs):
     vabs = np.linalg.norm(pVs, axis=1)
     sc=plt.scatter(pXs[:,0],pXs[:,1], c=vabs, cmap=plt.get_cmap('viridis'), vmin=0, vmax=max(vabs))
 
-def plot_data(pXs, pVs, fXs, fVs, i, image_folder, title, L, fix_frame=True, SAVEFIG=True, ex=None, plot_particles=True, plot_fluids=True, side_by_side=False, fluid_plot_type='streamplot', fluid_coloring_scheme='vabs'):
+def plot_data(pXs, pVs, fXs, fVs, sim_info, image_folder, title, L, fix_frame=True, SAVEFIG=True, ex=None, plot_particles=True, plot_fluids=True, side_by_side=False, fluid_plot_type='streamplot', fluid_coloring_scheme='vabs'):
     if not (plot_particles or plot_fluids):
         print('Plotting without anyting to plot. Raising exception...')
         raise RuntimeError
@@ -72,11 +72,11 @@ def plot_data(pXs, pVs, fXs, fVs, i, image_folder, title, L, fix_frame=True, SAV
     for ax in (axl,axr):
         plt.sca(ax)
         if fix_frame:
-            plt.xlim([-L,L])
-            plt.ylim([-L,L])
+            plt.xlim([sim_info['x_min'],sim_info['x_max']])
+            plt.ylim([sim_info['y_min'],sim_info['y_max']])
     plt.tight_layout()
 
-    IMG_NAME=f'{image_folder}/fig{i:08}.png'
+    IMG_NAME=f"{image_folder}/fig{sim_info['time_step_index']:08}.png"
     plt.savefig(IMG_NAME)
     if SAVEFIG:
         ex.add_artifact(IMG_NAME)
--- integrator.py
+++ integrator.py
@@ -4,12 +4,12 @@ from scipy.interpolate import RectBivariateSpline
 import matplotlib.pyplot as plt
 from pair_interactions import RHS
 
-def get_linear_grid(L,res=32):
-    return np.linspace(-L,L,res)
+def get_linear_grid(sim_info,res=32):
+    return np.linspace(sim_info['x_min'],sim_info['x_max'],res), np.linspace(sim_info['y_min'],sim_info['y_max'],res)
 
-def get_grid_pairs(L,res=32):
-    gridX = get_linear_grid(L,res)
-    XY = np.array(np.meshgrid(gridX,gridX)).reshape(2,res*res).T
+def get_grid_pairs(sim_info,res=32):
+    gridX, gridY = get_linear_grid(sim_info,res)
+    XY = np.array(np.meshgrid(gridX,gridY)).reshape(2,res*res).T
     return XY
 
 def fVs_on_points(ps, pXs, pVs, mu=1):
@@ -23,15 +23,15 @@ def fVs_on_points(ps, pXs, pVs, mu=1):
         fVs[ind] += np.outer(-np.log(lp),v) + np.multiply(dXp.T,np.dot(dXp,v)/lp**2).T
     return fVs/(8*np.pi*mu)
 
-def fVs_on_grid(pXs, pVs, L, mu=1, res=32):
-    fXs = get_grid_pairs(L, res)
+def fVs_on_grid(pXs, pVs, sim_info, mu=1, res=32):
+    fXs = get_grid_pairs(sim_info, res)
     return fXs, fVs_on_points(fXs, pXs, pVs, mu=mu)
 
-def fVs_on_particles(pXs, pVs, L, mu=1, res=32, spline_degree=3):
-    fXs_grid, fVs_grid = fVs_on_grid(pXs, pVs, L, mu, res=res)
-    gridX = get_linear_grid(L,res)
-    func_fV_x = RectBivariateSpline(gridX, gridX, fVs_grid[:,0].reshape(res,res).T, kx=spline_degree, ky=spline_degree) # Currently using a spline_degreerd degree splin
-    func_fV_y = RectBivariateSpline(gridX, gridX, fVs_grid[:,1].reshape(res,res).T, kx=spline_degree, ky=spline_degree)
+def fVs_on_particles(pXs, pVs, sim_info, mu=1, res=32, spline_degree=3):
+    fXs_grid, fVs_grid = fVs_on_grid(pXs, pVs, sim_info, mu, res=res)
+    gridX, gridY = get_linear_grid(sim_info,res)
+    func_fV_x = RectBivariateSpline(gridX, gridY, fVs_grid[:,0].reshape(res,res).T, kx=spline_degree, ky=spline_degree) # Currently using a spline_degreerd degree splin
+    func_fV_y = RectBivariateSpline(gridX, gridY, fVs_grid[:,1].reshape(res,res).T, kx=spline_degree, ky=spline_degree)
     fVs_x = func_fV_x.ev(pXs[:,0], pXs[:,1])
     fVs_y = func_fV_y.ev(pXs[:,0], pXs[:,1])
     return np.array((fVs_x,fVs_y)).T
@@ -67,20 +67,20 @@ def integrate_one_timestep(pXs, pVs, acc, activation_fn, sim_info, _config, get_
         pXs, pVs = create_and_destroy_particles(pXs, pVs, _config, sim_info)
     if Rdrag > 0:
         if use_interpolated_fluid_velocities:
-            fVs = fVs_on_particles(pXs, pVs, L=_config['L'], res=32, spline_degree=3, mu=mu)
+            fVs = fVs_on_particles(pXs, pVs, sim_info=sim_info, res=32, spline_degree=3, mu=mu)
         else:
             fVs = fVs_on_points(pXs, pXs, pVs, mu=mu)
         if DEBUG_INTERPOLATION:
             if use_interpolated_velocities:
                 fVs2 = fVs_on_points(pXs, pXs, pVs, mu=mu)
             else:
-                fVs2 = fVs_on_particles(pXs, pVs, _config['L'], res=32, spline_degree=3, mu=mu)
+                fVs2 = fVs_on_particles(pXs, pVs, sim_info=sim_info, res=32, spline_degree=3, mu=mu)
             plt.quiver(pXs[:,0],pXs[:,1], fVs [:,0], fVs [:,1], color='red')
             plt.quiver(pXs[:,0],pXs[:,1], fVs2[:,0], fVs2[:,1], color='green')
             plt.show(block=True)
         pVs += 6*np.pi*mu*Rdrag*fVs
     if get_fluid_velocity:
-        fXs, fVs = fVs_on_grid(pXs, pVs, _config['L'], mu=mu)
+        fXs, fVs = fVs_on_grid(pXs, pVs, sim_info=sim_info, mu=mu)
         return pXs, pVs, acc, fXs, fVs
     else:
         return pXs, pVs, acc, None, None
