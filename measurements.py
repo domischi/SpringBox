5ff3b01c1bf286ec87bec508dd30960d7158e852
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-24 17:46:10 -0700

    Add two simple measurements for the ratio of active particles

--- measurements.py
+++ measurements.py
@@ -1,6 +1,8 @@
 from illustration import plot_data
 
 def do_measurements(ex, _config, _run, sim_info, pXs, pVs, acc, ms, fXs, fVs, plotting_this_iteration):
+    _run.log_scalar("Ratio activated", sum(acc)/len(acc), sim_info['time_step_index'])
+    _run.log_scalar("Mass ratio activated", sum(ms*acc)/sum(ms), sim_info['time_step_index'])
     if plotting_this_iteration:
         plot_data(pXs, pVs, fXs, fVs,
                   sim_info,
