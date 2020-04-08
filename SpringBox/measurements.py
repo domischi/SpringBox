c2155d987dda5396956b6c38ec5f0c1f32b9aae8
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-04-08 14:08:11 -0700

    Add several useful scalar measurements

--- SpringBox/measurements.py
+++ SpringBox/measurements.py
@@ -7,25 +7,35 @@ def do_measurements(ex, _config, _run, sim_info, pXs, pVs, acc, ms, fXs, fVs, pl
     if acc is not None:
         _run.log_scalar("Ratio activated", sum(acc)/len(acc), sim_info['time_step_index'])
         _run.log_scalar("Mass ratio activated", sum(ms*acc)/sum(ms), sim_info['time_step_index'])
+        _run.log_scalar("Mass activated", sum(ms*acc), sim_info['time_step_index'])
 
     if pVs is not None:
         ## Avg velocity
         pV_avg = np.mean(pVs, axis=0)
         _run.log_scalar("pv_avg_x", pV_avg[0], sim_info['time_step_index'])
         _run.log_scalar("pv_avg_y", pV_avg[1], sim_info['time_step_index'])
+        _run.log_scalar("pv_avg_x-vx", pV_avg[0]-_config['window_velocity'][0], sim_info['time_step_index'])
+        _run.log_scalar("pv_avg_y-vy", pV_avg[1]-_config['window_velocity'][1], sim_info['time_step_index'])
 
     if fVs is not None:
         ## Max Velocity
         ind_fVs_max = np.argmax(np.linalg.norm(fVs,axis=1))
         _run.log_scalar("vmax_x", fVs[ind_fVs_max,0], sim_info['time_step_index'])
         _run.log_scalar("vmax_y", fVs[ind_fVs_max,1], sim_info['time_step_index'])
+        _run.log_scalar("vmax_x-vx", fVs[ind_fVs_max,0]-_config['window_velocity'][0], sim_info['time_step_index'])
+        _run.log_scalar("vmax_y-vy", fVs[ind_fVs_max,1]-_config['window_velocity'][1], sim_info['time_step_index'])
+
         _run.log_scalar("x_vmax", fXs[ind_fVs_max,0], sim_info['time_step_index'])
         _run.log_scalar("y_vmax", fXs[ind_fVs_max,1], sim_info['time_step_index'])
+        _run.log_scalar("x_vmax_c", fXs[ind_fVs_max,0]-(sim_info['x_max']+sim_info['x_min'])/2, sim_info['time_step_index'])
+        _run.log_scalar("y_vmax_c", fXs[ind_fVs_max,1]-(sim_info['x_max']+sim_info['x_min'])/2, sim_info['time_step_index'])
         
         ## Avg velocity
         fV_avg = np.mean(fVs, axis=0)
         _run.log_scalar("fv_avg_x", fV_avg[0], sim_info['time_step_index'])
         _run.log_scalar("fv_avg_y", fV_avg[1], sim_info['time_step_index'])
+        _run.log_scalar("fv_avg_x-vx", fV_avg[0]-_config['window_velocity'][0], sim_info['time_step_index'])
+        _run.log_scalar("fv_avg_y-vy", fV_avg[1]-_config['window_velocity'][1], sim_info['time_step_index'])
         
     if save_all_data_this_iteration:
         d= {
