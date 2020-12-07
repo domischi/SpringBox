a0ae915d6b05c91bc08e5fda8a0dd0d28cf00d49
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-12-07 10:25:22 -0800

    Change standard video format to gif

--- SpringBox/post_run_hooks.py
+++ SpringBox/post_run_hooks.py
@@ -1,5 +1,5 @@
-from .illustration import generate_video_from_png
+from .illustration import generate_gif_from_png
 def post_run_hooks(ex, _config, _run, working_folder):
     if _config['MAKE_VIDEO']:
-        video_path = generate_video_from_png(working_folder)
-        ex.add_artifact(video_path, name=f"video.avi")
+        video_path = generate_gif_from_png(working_folder)
+        ex.add_artifact(video_path, name=f"video.gif")
